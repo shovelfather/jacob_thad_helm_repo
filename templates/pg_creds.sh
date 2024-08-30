b797e437cdd58c61607dca7baab4daac516bad45
Shovelfather <shovelfather@gmail.com>
2024-08-29 20:00:17 -0400

    Initial commit
    squashme

---
+++ templates/pg_creds.sh
@@ -0,0 +1,5 @@
+DECISIONS_ADMINISTRATOR_EMAIL={ADMIN_EMAIL}
+DECISIONS_ADMINISTRATOR_PASSWORD={ADMIN_PASS}
+
+psql -c 'UPDATE entity_account SET password_hash = decode((select encode(sha256(\'$DECISIONS_ADMINISTRATOR_PASSWORD\'::bytea), \'hex\')), \'hex\') WHERE email_address = \'admin@decisions.com\';'
+psql -c 'UPDATE entity_account SET email_address = \'$DECISIONS_ADMINISTRATOR_PASSWORD\' WHERE email_address = \'admin@decisions.com\';'
